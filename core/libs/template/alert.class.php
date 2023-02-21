fc79b548c3a8e8121d8362aab1bc3edfa1c2e51d
Nadarajan GS <nadarajangs31@gmail.com>
2023-02-21 05:33:29 +0000

    core template

---
+++ core/libs/template/alert.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class Alert
+{
+  public static function delete()
+  {
+    echo '
+        <div class="alert alert-success mb-4" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+          <strong>Deleted!</strong> successfully.
+        </div>';
+  }
+
+  public static function update()
+  {
+    echo ' 
+        <div class="alert alert-primary mb-4" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+          <strong>Updated!</strong> successfully.
+        </div>';
+  }
+
+  public static function insert()
+  {
+    echo ' 
+        <div class="alert alert-primary mb-4" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+          <strong>Inserted!</strong> successfully.
+        </div>';
+  }
+
+  public static function custom($msg)
+  {
+    echo "
+        <div class='alert alert-danger mb-4' role='alert'>
+          <button type='button' class='close' data-dismiss='alert' aria-label='Close'><svg> ... </svg></button>
+          <strong> $msg </strong> .
+        </div>";
+  }
+
+
+  public static function Success()
+  {
+    echo ' 
+        <div class="alert alert-success mb-4" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><svg> ... </svg></button>
+          <strong> Action </strong> Done successfully.
+        </div>';
+  }
+
+}
